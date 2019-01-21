b977c2c027de2c59fa2a46591f8b1883eb4a9369
rk <rkmbp@rkui-MacBook-Pro.local>
2019-01-22 03:39:17 +0900

    play sound on collisions

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,9 +6,12 @@ public class PlayerController : MonoBehaviour {
 
 	private Rigidbody2D rb2d;
 	private float speed = 2;
+	//sound
+	private AudioSource bumpSource;
 
 	void Start () {
 		rb2d = GetComponent<Rigidbody2D> ();
+		bumpSource = GetComponent<AudioSource> ();
 	}
 	
 	void FixedUpdate () {
@@ -16,5 +19,15 @@ public class PlayerController : MonoBehaviour {
 		float moveVertical = Input.GetAxis("Vertical");
 		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
 		rb2d.velocity = movement * speed;
+	}
+
+	void OnCollisionEnter2D(Collision2D col) {
+		if(col.gameObject.tag == "Wall") {
+			if (col.relativeVelocity.magnitude > 1){
+				bumpSource.Play();
+			}
+		}
+		// print("collision detected. play sound. ");
+		// Destroy(collision.gameObject);
 	}
 }
