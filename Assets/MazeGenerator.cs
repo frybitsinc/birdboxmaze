0916e27938cfbb909caebe44e2cddbaf74f107f7
rk <rkmbp@rkui-MacBook-Pro.local>
2019-01-24 20:08:59 +0900

    add mainmenu scene

--- Assets/MazeGenerator.cs
+++ Assets/MazeGenerator.cs
@@ -77,7 +77,7 @@ public class MazeGenerator : MonoBehaviour {
      */
     private void Start()
     {
-        GenerateMaze(mazeRows, mazeColumns);
+        //GenerateMaze(mazeRows, mazeColumns);
         //remove centre cell walls
         RemoveWall(centreCells[0].cScript, 1);
         RemoveWall(centreCells[0].cScript, 2);
